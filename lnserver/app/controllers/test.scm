f4159bfd27ec124d2bbd9459d1330b094a379689
mbcladwell <mbcladwell@stihie.net>
2021-03-24 14:45:56 -0400

    modified test

--- lnserver/app/controllers/test.scm
+++ lnserver/app/controllers/test.scm
@@ -3,29 +3,18 @@
 ;; This file is generated automatically by GNU Artanis.
 (define-artanis-controller test) ; DO NOT REMOVE THIS LINE!!!
 
-(test-define add
+(test-define page1
   (lambda (rc)
-  "<h1>This is test#add</h1><p>Find me in app/views/test/add.html.tpl</p>"
-  ;; TODO: add controller method `add'
-  ;; uncomment this line if you want to render view from template
-  ;; (view-render "add" (the-environment))
+   (view-render "page1" (the-environment))
   ))
 
-(test-define remove
-  (options #:cookies '(names prjid sid))
+(post "/test/page1action"
 		 (lambda (rc)
-		   (let* (
-			  (dummy (:cookies-setattr! rc 'prjid #:path "/plateset"))
-			  (dummy (:cookies-remove! rc "prjid"))
-			  )
-		     (view-render "remove" (the-environment))
-		     )))
+		     (redirect-to rc "/test/page2")
+		     ))
 
-(test-define check
+(test-define page2
   (lambda (rc)
-  "<h1>This is test#check</h1><p>Find me in app/views/test/check.html.tpl</p>"
-  ;; TODO: add controller method `check'
-  ;; uncomment this line if you want to render view from template
-  ;; (view-render "check" (the-environment))
+   (view-render "page2" (the-environment))
   ))
 
