a848b9f0f7f533c6c4e86f83a4616990ab53a295
mbcladwell <mbcladwell@stihie.net>
2021-03-30 12:00:12 -0400

    localhost for aws

--- lnserver/app/controllers/test.scm
+++ lnserver/app/controllers/test.scm
@@ -2,6 +2,11 @@
 ;; Please add your license header here.
 ;; This file is generated automatically by GNU Artanis.
 (define-artanis-controller test) ; DO NOT REMOVE THIS LINE!!!
+(use-modules (web uri))
+
+(define (get-redirect-uri dest)
+  (string->uri (string-append "http://3.13.182.53:3000" dest)))
+
 
 (test-define page1
   (lambda (rc)
@@ -10,7 +15,7 @@
 
 (post "/test/page1action"
 		 (lambda (rc)
-		     (redirect-to rc "/test/page2")
+		     (redirect-to rc (get-redirect-uri "/test/page2"))
 		     ))
 
 (test-define page2
